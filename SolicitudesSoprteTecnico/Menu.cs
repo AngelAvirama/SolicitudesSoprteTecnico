15d78f78a431c53e7db8627a8093904519b636ba
AngelAvirama <142739887+AngelAvirama@users.noreply.github.com>
2023-09-13 22:34:19 -0500

    Add files via upload

---
+++ SolicitudesSoprteTecnico/Menu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestsQueue
+{
+    public static class Menu
+    {
+        public static void ChooseOptionMenu(int auto = -1) //If auto is -1 we ask for an input
+        {
+            int count_option = 1;//in case int auto is not given
+            int option = 0;
+
+            bool continueMenu = true; 
+
+            //for instance
+            while (continueMenu)
+            {
+                if (auto == -1)
+                {
+                    ShowMenu();
+                    option = Convert.ToInt32(Console.ReadLine()); //Using inputs
+                }
+                
+                else if (count_option == auto * 2+1) // half enqueuing and half dequeuing
+                {
+                    continueMenu = false;
+
+
+                }
+                else if(count_option <= auto  )
+                {
+                    option = 1; //Adding a request  automatically
+                    Console.WriteLine(count_option);
+
+                }
+                else
+                {
+                    option = 2; //Attending to requests automatically
+                    Console.WriteLine(count_option);
+                }
+                count_option++;
+                switch (option)
+                {
+                    case 1:
+
+                        Technical_Support.AddRequest();
+                        break;
+                    case 2:
+                        Technical_Support.AttendToRequests(); 
+                        break;
+                    case 3:
+                        Technical_Support.ShowRequests();
+                        break;
+                    case 4: 
+                        Technical_Support.UpdateUrgencyLevel();
+                        break;
+                    case 5:
+                        continueMenu = false;
+                        break;
+                }
+            }
+
+        }
+
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Technical Support:\n" +
+                "\n1.Add Request" +
+                "\n2.Attend To Requests" +
+                "\n3.Show Requests" +
+                "\n4.Update Urgency Level" +
+                "\n5.Exit" +
+                "\nChoose an option:");
+        }
+    }
+}
--- SolicitudesSoprteTecnico/Program.cs
+++ SolicitudesSoprteTecnico/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics; // For measuring execution time
+namespace RequestsQueue
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<String> StringDataRequests = FileHandling.ReadFile("C:\\Users\\avira\\source\\repos\\RequestsQueue\\ColaSolicitudes\\data.txt");
+
+
+
+           
+
+           
+            Console.WriteLine("enter -1 for manual execution. Otherwise, select 10, 100, 1000 or 10000");
+            int autoOption = Convert.ToInt32(Console.ReadLine());
+
+            Menu.ChooseOptionMenu(autoOption);
+
+            
+            
+
+           
+
+            
+            
+        }
+    }
+}
